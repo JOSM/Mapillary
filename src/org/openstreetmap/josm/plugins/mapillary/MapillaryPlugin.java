af3487dd500c511d0b92b5036d9fd145b94c1cdd
peterneubauer <peter@neubauer.se>
2016-06-17 09:10:38 +0200

    merging

--- src/org/openstreetmap/josm/plugins/mapillary/MapillaryPlugin.java
+++ src/org/openstreetmap/josm/plugins/mapillary/MapillaryPlugin.java
@@ -26,10 +26,7 @@ import org.openstreetmap.josm.plugins.mapillary.actions.MapillaryJoinAction;
 import org.openstreetmap.josm.plugins.mapillary.actions.MapillaryUploadAction;
 import org.openstreetmap.josm.plugins.mapillary.actions.MapillaryWalkAction;
 import org.openstreetmap.josm.plugins.mapillary.actions.MapillaryZoomAction;
-import org.openstreetmap.josm.plugins.mapillary.gui.MapillaryFilterDialog;
-import org.openstreetmap.josm.plugins.mapillary.gui.MapillaryHistoryDialog;
-import org.openstreetmap.josm.plugins.mapillary.gui.MapillaryMainDialog;
-import org.openstreetmap.josm.plugins.mapillary.gui.MapillaryPreferenceSetting;
+import org.openstreetmap.josm.plugins.mapillary.gui.*;
 import org.openstreetmap.josm.plugins.mapillary.io.download.MapillaryDownloader;
 import org.openstreetmap.josm.plugins.mapillary.oauth.MapillaryUser;
 import org.openstreetmap.josm.tools.ImageProvider;
@@ -218,6 +215,7 @@ public class MapillaryPlugin extends Plugin {
     if (oldFrame == null && newFrame != null) { // map frame added
       Main.map.addToggleDialog(MapillaryMainDialog.getInstance(), false);
       Main.map.addToggleDialog(MapillaryHistoryDialog.getInstance(), false);
+      Main.map.addToggleDialog(MapillaryChangesetDialog.getInstance(), false);
       Main.map.addToggleDialog(MapillaryFilterDialog.getInstance(), false);
       setMenuEnabled(downloadMenu, true);
       if (MapillaryDownloader.getMode() == MapillaryDownloader.MODES.Manual)
@@ -228,6 +226,7 @@ public class MapillaryPlugin extends Plugin {
     if (oldFrame != null && newFrame == null) { // map frame destroyed
       MapillaryMainDialog.destroyInstance();
       MapillaryHistoryDialog.destroyInstance();
+      MapillaryChangesetDialog.destroyInstance();
       MapillaryFilterDialog.destroyInstance();
       setMenuEnabled(downloadMenu, false);
       setMenuEnabled(downloadViewMenu, false);
