7e0b26423d71da0d0e212aa2b9b7bd0b4199a891
Florian Schäfer <florian@schaeferban.de>
2016-10-11 20:37:54 +0200

    Make MapillaryImportIntoSequenceAction a subclass of MapillaryImportAction
    This avoids duplication of code across these two classes. The common code is now in the methods MapillaryImportAction.chooseImages(), MapillaryImportAction.recordChanges(List<MapillaryAbstractImage>) and inside the new protected constructor.

--- src/org/openstreetmap/josm/plugins/mapillary/actions/MapillaryImportIntoSequenceAction.java
+++ src/org/openstreetmap/josm/plugins/mapillary/actions/MapillaryImportIntoSequenceAction.java
@@ -4,29 +4,12 @@ package org.openstreetmap.josm.plugins.mapillary.actions;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
-import java.io.File;
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.ConcurrentSkipListSet;
 
-import javax.swing.JFileChooser;
-
-import org.openstreetmap.josm.Main;
-import org.openstreetmap.josm.actions.JosmAction;
-import org.openstreetmap.josm.data.preferences.StringProperty;
 import org.openstreetmap.josm.plugins.mapillary.MapillaryAbstractImage;
-import org.openstreetmap.josm.plugins.mapillary.MapillaryPlugin;
 import org.openstreetmap.josm.plugins.mapillary.MapillarySequence;
-import org.openstreetmap.josm.plugins.mapillary.history.MapillaryRecord;
-import org.openstreetmap.josm.plugins.mapillary.history.commands.CommandImport;
-import org.openstreetmap.josm.plugins.mapillary.utils.ImageUtil;
-import org.openstreetmap.josm.plugins.mapillary.utils.MapillaryUtils;
-import org.openstreetmap.josm.tools.Shortcut;
 
 /**
  * Imports a set of images and puts them in a single {@link MapillarySequence}.
@@ -34,69 +17,34 @@ import org.openstreetmap.josm.tools.Shortcut;
  * @author nokutu
  *
  */
-public class MapillaryImportIntoSequenceAction extends JosmAction {
+public class MapillaryImportIntoSequenceAction extends MapillaryImportAction {
+  private static final long serialVersionUID = 7892328318806455809L;
 
-  private static final long serialVersionUID = -9190217809965894878L;
-
-  private List<MapillaryAbstractImage> images;
-  private final StringProperty startDirProp =
-    new StringProperty("mapillary.start-directory", System.getProperty("user.home"));
-
-  /**
-   * Main constructor.
-   */
   public MapillaryImportIntoSequenceAction() {
-    super(tr("Import pictures into sequence"), MapillaryPlugin
-        .getProvider("icon24.png"), tr("Import local pictures"),
-        Shortcut.registerShortcut("Import Mapillary Sequence", tr("Import pictures into Mapillary layer in a sequence"),
-            KeyEvent.CHAR_UNDEFINED, Shortcut.NONE), false,
-        "mapillaryImportSequence", false);
-    this.setEnabled(false);
+    super(
+      tr("Import pictures into sequence"),
+      tr("Import local pictures"),
+      "Import Mapillary Sequence",
+      tr("Import pictures into Mapillary layer in a sequence"),
+      "mapillaryImportSequence"
+    );
   }
 
   @Override
   public void actionPerformed(ActionEvent arg0) {
-    this.images = new ArrayList<>();
-
-    JFileChooser chooser = new JFileChooser();
-    chooser.setCurrentDirectory(new File(startDirProp.get()));
-    chooser.setDialogTitle(tr("Select pictures"));
-    chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-    chooser.setAcceptAllFileFilterUsed(false);
-    chooser.addChoosableFileFilter(ImageUtil.IMAGE_FILE_FILTER);
-    chooser.setMultiSelectionEnabled(true);
-
-    if (chooser.showOpenDialog(Main.parent) == JFileChooser.APPROVE_OPTION) {
-
-      for (File file : chooser.getSelectedFiles()) {
-        try {
-          images.addAll(ImageUtil.readImagesFrom(
-              file,
-              Main.map.mapView.getProjection().eastNorth2latlon(Main.map.mapView.getCenter())
-          ));
-        } catch (IOException e) {
-          Main.error("Could not read image(s) from "+file.getAbsolutePath());
-        }
-      }
-      if (chooser.getSelectedFiles().length >= 1) {
-        final File lastSelectedFile = chooser.getSelectedFiles()[chooser.getSelectedFiles().length - 1];
-        startDirProp.put(
-          lastSelectedFile.getParent() == null ? lastSelectedFile.getAbsolutePath() : lastSelectedFile.getParent()
-        );
-      }
-      joinImages();
-      MapillaryRecord.getInstance().addCommand(new CommandImport(new ConcurrentSkipListSet<>(images)));
-    }
-    MapillaryUtils.showAllPictures();
+    List<MapillaryAbstractImage> images = chooseImages();
+    joinImages(images);
+    recordChanges(images);
   }
 
   /**
-   * Joins all the images in a unique {@link MapillarySequence}.
+   * Joins all the images into a new {@link MapillarySequence} (in chronological order).
+   * @param images the images that should be joined
    */
-  public void joinImages() {
-    Collections.sort(this.images, new MapillaryEpochComparator());
+  public void joinImages(List<MapillaryAbstractImage> images) {
+    Collections.sort(images, new MapillaryEpochComparator());
     MapillarySequence seq = new MapillarySequence();
-    for (MapillaryAbstractImage img : this.images) {
+    for (MapillaryAbstractImage img : images) {
       seq.add(img);
       img.setSequence(seq);
     }
@@ -105,20 +53,13 @@ public class MapillaryImportIntoSequenceAction extends JosmAction {
   /**
    * Comparator that compares two {@link MapillaryAbstractImage} objects
    * depending on the time they were taken.
-   *
-   * @author nokutu
-   *
    */
-  public static class MapillaryEpochComparator implements
-      Comparator<MapillaryAbstractImage>, Serializable {
-
+  public static class MapillaryEpochComparator implements Comparator<MapillaryAbstractImage> {
     @Override
     public int compare(MapillaryAbstractImage arg0, MapillaryAbstractImage arg1) {
-      if (arg0.getCapturedAt() < arg1.getCapturedAt())
-        return -1;
-      if (arg0.getCapturedAt() > arg1.getCapturedAt())
-        return 1;
-      return 0;
+      return arg0.getCapturedAt() == arg1.getCapturedAt()
+        ? 0
+        : (int) Math.copySign(1, arg0.getCapturedAt() - arg1.getCapturedAt());
     }
   }
 }
