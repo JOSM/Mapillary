f09f87cb41e5e1c90022a10b15d53f0c9f146d30
Florian Schäfer <florian@schaeferban.de>
2016-11-16 06:33:15 +0100

    Disable moving and turning of already uploaded images again for non-developers
    The API is not yet ready for uploading changesets, so this is now only enabled for developers.

---
+++ src/org/openstreetmap/josm/plugins/mapillary/utils/MapillaryProperties.java
@@ -0,0 +1,8 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins.mapillary.utils;
+
+import org.openstreetmap.josm.data.preferences.BooleanProperty;
+
+public final class MapillaryProperties {
+  public static final BooleanProperty DEVELOPER = new BooleanProperty("mapillary.developer", false);
+}
