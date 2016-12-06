78c077044142dddfc46908e0f3e2897ebab949ed
Kamil Nikel <knikel@posteo.de>
2016-12-06 16:03:08 +0100

    Prepare fro npm publish

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare module 'mapillary_sprite_source' {
+
+  export let mapping: { [index: string]: {
+    regions: string[]
+  } };
+
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+
+var mappingJson = fs.readFileSync('./mapping.json', 'utf8');
+
+module.exports = {
+    mapping: mappingJson,
+};
