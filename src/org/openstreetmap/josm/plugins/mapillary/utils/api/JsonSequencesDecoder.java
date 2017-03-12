76e77944d7c2a575cdf72b3ace0d87869d512736
Florian Schäfer <florian@schaeferban.de>
2017-03-12 19:59:16 +0100

    Add a class for decoding the JSON from APIv3, which represents sequences
    This is for easy and reliable creation of Java objects from responses of the v3 API.
    The class is accompanied by unit tests, which provide 100% instruction coverage and 100% branch coverage of the new class.

---
+++ src/org/openstreetmap/josm/plugins/mapillary/utils/api/JsonSequencesDecoder.java
@@ -0,0 +1,160 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins.mapillary.utils.api;
+
+import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.function.Function;
+
+import javax.json.JsonArray;
+import javax.json.JsonNumber;
+import javax.json.JsonObject;
+import javax.json.JsonString;
+import javax.json.JsonValue;
+import javax.json.JsonValue.ValueType;
+
+import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.data.coor.LatLon;
+import org.openstreetmap.josm.plugins.mapillary.MapillaryImage;
+import org.openstreetmap.josm.plugins.mapillary.MapillarySequence;
+
+/**
+ * The JsonSequencesDecoder decodes the JSON returned by APIv3 into Java objects
+ */
+public final class JsonSequencesDecoder {
+  private JsonSequencesDecoder() {
+    // Private constructor to avoid instantiation
+  }
+
+  /**
+   * Parses a given {@link JsonObject} as a GeoJSON FeatureCollection into a {@link Collection}
+   * of multiple {@link MapillarySequence}.
+   * @param json the {@link JsonObject} to be parsed
+   * @return a {@link Collection} of {@link MapillarySequence}s that are parsed from the given {@link JsonObject}.
+   *         Currently a {@link HashMap} is used, but please don't rely on it, this could change at any time without
+   *         prior notice. The return value will not be <code>null</code>.
+   */
+  public static Collection<MapillarySequence> decodeSequences(final JsonObject json) {
+    final Collection<MapillarySequence> result = new HashSet<>();
+    if (
+      json != null && "FeatureCollection".equals(json.getString("type", null)) &&
+      json.containsKey("features") && json.get("features").getValueType() == ValueType.ARRAY
+    ) {
+      final JsonArray features = json.getJsonArray("features");
+      for (int i = 0; i < features.size(); i++) {
+        final MapillarySequence seq = decodeSequence(features.getJsonObject(i));
+        if (seq != null) {
+          result.add(seq);
+        }
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Parses a given {@link JsonObject} as a GeoJSON Feature into a {@link MapillarySequence}.
+   * @param json the {@link JsonObject} to be parsed
+   * @return a {@link MapillarySequence} that is parsed from the given {@link JsonObject}. If mandatory information is
+   *         missing from the JSON or it's not meeting the expecting format in another way, <code>null</code> will be
+   *         returned.
+   */
+  public static MapillarySequence decodeSequence(final JsonObject json) {
+    MapillarySequence result = null;
+    if ("Feature".equals(json.getString("type", null))) {
+      final JsonObject properties = json.getJsonObject("properties");
+      try {
+        if (properties != null && properties.getString("key", null) != null && properties.getString("captured_at", null) != null) {
+          result = new MapillarySequence(
+            properties.getString("key", null),
+            new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX", Locale.UK).parse(properties.getString("captured_at", null)).getTime()
+          );
+          Double[] cas = decodeCoordinateProperty(properties, "cas", (val) -> { return val instanceof JsonNumber ? ((JsonNumber)val).doubleValue() : null; }, Double.class);
+          String[] imageKeys = decodeCoordinateProperty(properties, "image_keys", (val) -> { return val instanceof JsonString ? ((JsonString) val).getString() : null;}, String.class);
+          LatLon[] geometry = decodeLatLons(json.getJsonObject("geometry"));
+          final int sequenceLength = Math.min(Math.min(cas.length, imageKeys.length), geometry.length);
+          for (int i = 0; i < sequenceLength; i++) {
+            if (cas[i] != null && imageKeys[i] != null && geometry[i] != null) {
+              result.add(new MapillaryImage(imageKeys[i], geometry[i], cas[i]));
+            }
+          }
+          if (result.getImages().size() <= 0) {
+            result = null;
+          }
+        }
+      } catch (ParseException e) {
+        Main.warn(e, "Invalid date `" + properties.getString("captured_at", null) +
+          "` for a sequence returned by the API, which could not be decoded!");
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Converts a {@link JsonArray} to a java array.
+   * The conversion from {@link JsonValue} to a java type is done by the supplied function.
+   * @param array the array to be converted
+   * @param decodeValueFunction the function used for conversion from {@link JsonValue} to the desired type.
+   * @param clazz the desired type that the elements of the resulting array should have
+   * @return the supplied array converted from {@link JsonArray} to a java array of the supplied type, converted using
+   *         the supplied fuction. Never <code>null</code>, in case of array==null, an array of length 0 is returned.
+   */
+  private static <T> T[] decodeJsonArray(final JsonArray array, final Function<JsonValue, T> decodeValueFunction, final Class<T> clazz) {
+    @SuppressWarnings("unchecked")
+    final T[] result = (T[]) Array.newInstance(clazz, array == null ? 0 : array.size());
+    for (int i = 0; i < result.length; i++) {
+      result[i] = decodeValueFunction.apply(array.get(i));
+    }
+    return result;
+  }
+
+  /**
+   * Given the JSON object representing the `properties` of a sequence, this method converts you one attribute from the
+   * `coordinateProperties` object to an array of appropriate type.
+   *
+   * For example this is used to convert the `image_keys` JSON array to a String[] array or the `cas` JSON array to a
+   * Double[] array.
+   * @param json the JSON object representing the `properties` of a sequence
+   * @param key the key, which identifies the desired array inside the `coordinateProperties` object to be converted
+   * @param decodeValueFunction a function that converts the {@link JsonValue}s in the JSON array to java objects of the
+   *        desired type
+   * @param clazz the {@link Class} object of the desired type, that the entries of the resulting array should have
+   * @return the resulting array, when converting the desired entry of the `coordinateProperties`.
+   *         Never <code>null</code>. If no `coordinateProperties` are set, or if the desired key is not set or is not
+   *         an array, then an empty array of the desired type is returned.
+   */
+  @SuppressWarnings("unchecked")
+  private static <T> T[] decodeCoordinateProperty(final JsonObject json, final String key, final Function<JsonValue, T> decodeValueFunction, final Class<T> clazz) {
+    final JsonValue coordinateProperties = json.get("coordinateProperties");
+    if (coordinateProperties instanceof JsonObject) {
+      JsonValue valueArray = ((JsonObject) coordinateProperties).get(key);
+      if (valueArray instanceof JsonArray) {
+        return decodeJsonArray((JsonArray) valueArray, decodeValueFunction, clazz);
+      }
+    }
+    return (T[]) Array.newInstance(clazz, 0);
+  }
+
+  private static LatLon[] decodeLatLons(final JsonObject json) {
+    final JsonValue coords = json == null ? null : json.get("coordinates");
+    if (coords instanceof JsonArray && "LineString".equals(json.getString("type", null))) {
+      final LatLon[] result = new LatLon[((JsonArray)coords).size()];
+      for (int i = 0; i < ((JsonArray)coords).size(); i++) {
+        final JsonValue coord = ((JsonArray)coords).get(i);
+        if (
+          coord instanceof JsonArray &&
+          ((JsonArray)coord).size() == 2 &&
+          ((JsonArray)coord).get(0) instanceof JsonNumber &&
+          ((JsonArray)coord).get(1) instanceof JsonNumber
+        ) {
+          result[i] = new LatLon(((JsonArray)coord).getJsonNumber(0).doubleValue(), ((JsonArray)coord).getJsonNumber(1).doubleValue());
+        }
+      }
+      return result;
+    }
+    return new LatLon[0];
+  }
+}
---
+++ test/unit/org/openstreetmap/josm/plugins/mapillary/utils/api/JsonSequencesDecoderTest.java
@@ -0,0 +1,180 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins.mapillary.utils.api;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.io.ByteArrayInputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.function.Function;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonValue;
+
+import org.junit.Test;
+import org.openstreetmap.josm.data.coor.LatLon;
+import org.openstreetmap.josm.plugins.mapillary.MapillaryImage;
+import org.openstreetmap.josm.plugins.mapillary.MapillarySequence;
+import org.openstreetmap.josm.plugins.mapillary.utils.TestUtil;
+
+/**
+ *
+ */
+public class JsonSequencesDecoderTest {
+
+  @Test
+  public void testDecodeSequences() {
+    Collection<MapillarySequence> exampleSequences = JsonSequencesDecoder.decodeSequences(
+      Json.createReader(this.getClass().getResourceAsStream("/api/v3/responses/searchSequences.json")).readObject()
+    );
+    assertNotNull(exampleSequences);
+    assertEquals(1, exampleSequences.size());
+    MapillarySequence seq = exampleSequences.iterator().next();
+    assertEquals(4, seq.getImages().size());
+
+    assertEquals("LwrHXqFRN_pszCopTKHF_Q", ((MapillaryImage) seq.getImages().get(0)).getKey());
+    assertEquals("Aufjv2hdCKwg9LySWWVSwg", ((MapillaryImage) seq.getImages().get(1)).getKey());
+    assertEquals("QEVZ1tp-PmrwtqhSwdW9fQ", ((MapillaryImage) seq.getImages().get(2)).getKey());
+    assertEquals("G_SIwxNcioYeutZuA8Rurw", ((MapillaryImage) seq.getImages().get(3)).getKey());
+
+    assertEquals(323.0319999999999, seq.getImages().get(0).getCa(), 1e-10);
+    assertEquals(320.8918, seq.getImages().get(1).getCa(), 1e-10);
+    assertEquals(333.62239999999997, seq.getImages().get(2).getCa(), 1e-10);
+    assertEquals(329.94820000000004, seq.getImages().get(3).getCa(), 1e-10);
+
+    assertEquals(new LatLon(16.432958, 7.246497),  seq.getImages().get(0).getLatLon());
+    assertEquals(new LatLon(16.432955, 7.246567),  seq.getImages().get(1).getLatLon());
+    assertEquals(new LatLon(16.432971, 7.248372),  seq.getImages().get(2).getLatLon());
+    assertEquals(new LatLon(16.432976, 7.249027),  seq.getImages().get(3).getLatLon());
+
+    assertEquals(1_457_963_093_860l, seq.getCapturedAt()); // 2016-03-14T13:44:53.860 UTC
+  }
+
+  @Test
+  public void testDecodeSequencesInvalid() {
+    // null input
+    assertEquals(0, JsonSequencesDecoder.decodeSequences(null).size());
+    // empty object
+    assertNumberOfDecodedSequences(0, "{}");
+    // object without type=FeatureCollection
+    assertNumberOfDecodedSequences(0, "{\"features\": []}");
+    // object with wrong value for the type attribute
+    assertNumberOfDecodedSequences(0, "{\"type\": \"SomethingArbitrary\", \"features\": []}");
+    // object without features-array
+    assertNumberOfDecodedSequences(0, "{\"type\": \"FeatureCollection\"}");
+    // object with a value for the features-key, but wrong type (in this case string instead of Array)
+    assertNumberOfDecodedSequences(0, "{\"type\": \"FeatureCollection\", \"features\": \"notAnArray\"}");
+  }
+
+  @Test
+  public void testDecodeSequencesWithArbitraryObjectAsFeature() {
+    assertNumberOfDecodedSequences(0, "{\"type\": \"FeatureCollection\", \"features\": [{}]}");
+  }
+
+  private static void assertNumberOfDecodedSequences(int expectedNumberOfSequences, String jsonString) {
+    assertEquals(
+      expectedNumberOfSequences,
+      JsonSequencesDecoder.decodeSequences(
+        Json.createReader(new ByteArrayInputStream(jsonString.getBytes())).readObject()
+      ).size()
+    );
+  }
+
+  @Test
+  public void testDecodeSequence() {
+    MapillarySequence exampleSequence = JsonSequencesDecoder.decodeSequence(
+      Json.createReader(this.getClass().getResourceAsStream("/api/v3/responses/sequence.json")).readObject()
+    );
+    assertEquals("cHBf9e8n0pG8O0ZVQHGFBQ", exampleSequence.getKey());
+    assertEquals(1_457_963_077_206l, exampleSequence.getCapturedAt()); // 2016-03-14T13:44:37.206 UTC
+    assertEquals(2, exampleSequence.getImages().size());
+
+    assertEquals(
+      new MapillaryImage("76P0YUrlDD_lF6J7Od3yoA", new LatLon(16.43279, 7.246085), 96.71454),
+      exampleSequence.getImages().get(0)
+    );
+    assertEquals(
+      new MapillaryImage("Ap_8E0BwoAqqewhJaEbFyQ", new LatLon(16.432799, 7.246082), 96.47705000000002),
+      exampleSequence.getImages().get(1)
+    );
+  }
+
+  @Test
+  public void testDecodeSequenceInvalid() {
+    // `properties` key is not set
+    assertNull(JsonSequencesDecoder.decodeSequence(Json.createReader(new ByteArrayInputStream(
+      "{\"type\": \"Feature\"}".getBytes()
+    )).readObject()));
+    // value for the key `key` in the properties is missing
+    assertNull(JsonSequencesDecoder.decodeSequence(Json.createReader(new ByteArrayInputStream(
+      "{\"type\": \"Feature\", \"properties\": {\"captured_at\": \"1970-01-01T00:00:00.000Z\"}}".getBytes()
+    )).readObject()));
+    // value for the key `captured_at` in the properties is missing
+    assertNull(JsonSequencesDecoder.decodeSequence(Json.createReader(new ByteArrayInputStream(
+      "{\"type\": \"Feature\", \"properties\": {\"key\": \"someKey\"}}".getBytes()
+    )).readObject()));
+    // the date in `captured_at` has an unrecognized format
+    assertNull(JsonSequencesDecoder.decodeSequence(Json.createReader(new ByteArrayInputStream(
+      "{\"type\": \"Feature\", \"properties\": {\"key\": \"someKey\", \"captured_at\": \"unrecognizedDateFormat\"}}".getBytes()
+    )).readObject()));
+    // `coordinateProperties` have unexpected value (in this case null)
+    assertNull(JsonSequencesDecoder.decodeSequence(Json.createReader(new ByteArrayInputStream(
+      "{\"type\": \"Feature\", \"properties\": {\"key\": \"someKey\", \"captured_at\": \"1970-01-01T00:00:00.000Z\", \"coordinateProperties\": null}}".getBytes()
+    )).readObject()));
+    // the `image_key` array and the `cas` array contain unexpected values (in this case `null`)
+    assertNull(JsonSequencesDecoder.decodeSequence(Json.createReader(new ByteArrayInputStream(
+      "{\"type\": \"Feature\", \"properties\": {\"key\": \"someKey\", \"captured_at\": \"1970-01-01T00:00:00.000Z\", \"coordinateProperties\": {\"cas\": null, \"image_keys\": null}}}".getBytes()
+    )).readObject()));
+    // the `image_key` array and the `cas` array contain unexpected values (in this case `null`)
+    assertNull(JsonSequencesDecoder.decodeSequence(Json.createReader(new ByteArrayInputStream(
+      "{\"type\": \"Feature\", \"properties\": {\"key\": \"someKey\", \"captured_at\": \"1970-01-01T00:00:00.000Z\", \"coordinateProperties\": {\"cas\": [null, null, null, null, 1.0, 1.0, 1.0], \"image_keys\": [null, null, \"key\", \"key\", null, null, \"key\"]}}, \"geometry\": {\"type\": \"LineString\", \"coordinates\": [null, [1,1], null, [1,1], null, [1,1], null]}}".getBytes()
+    )).readObject()));
+  }
+
+  /**
+   * Checks if an empty array is returned, if <code>null</code> is supplied to the method as the array.
+   * @throws NoSuchMethodException
+   * @throws SecurityException
+   * @throws IllegalAccessException
+   * @throws IllegalArgumentException
+   * @throws InvocationTargetException
+   */
+  @Test
+  public void testDecodeJsonArray() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    Method method = JsonSequencesDecoder.class.getDeclaredMethod("decodeJsonArray", JsonArray.class, Function.class, Class.class);
+    method.setAccessible(true);
+    assertEquals(0, ((String[])method.invoke(null, null, (Function<JsonValue, String>) ((val) -> { return null; }), String.class)).length);
+  }
+
+  /**
+   * Checks if an empty array is returned, if <code>null</code> is supplied to the method as the array.
+   * @throws NoSuchMethodException
+   * @throws SecurityException
+   * @throws IllegalAccessException
+   * @throws IllegalArgumentException
+   * @throws InvocationTargetException
+   */
+  @Test
+  public void testDecodeLatLons() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    Method method = JsonSequencesDecoder.class.getDeclaredMethod("decodeLatLons", JsonObject.class);
+    method.setAccessible(true);
+    assertEquals(0, ((LatLon[]) method.invoke(null, Json.createReader(new ByteArrayInputStream("{\"type\": \"Feature\", \"coordinates\": []}".getBytes())).readObject())).length);
+
+    LatLon[] example = (LatLon[]) method.invoke(null, Json.createReader(new ByteArrayInputStream("{\"type\": \"LineString\", \"coordinates\": [ [1,2,3], [\"a\", 2], [1, \"b\"] ]}".getBytes())).readObject());
+    assertEquals(3, example.length);
+    assertNull(example[0]);
+    assertNull(example[1]);
+    assertNull(example[2]);
+  }
+
+  @Test
+  public void testUtilityClass() {
+    TestUtil.testUtilityClass(JsonSequencesDecoder.class);
+  }
+
+}
