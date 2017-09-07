JOSM/Mapillary

commit e9ed8ba3b1ec96e8824894e0567dc0377ef32703

Improve synchronization

src/org/openstreetmap/josm/plugins/mapillary/MapillaryData.java
@@ -83,9 +83,7 @@ public class MapillaryData {
    * @throws NullPointerException if parameter <code>image</code> is <code>null</code>
    */
   public void add(MapillaryAbstractImage image, boolean update) {
-    synchronized (images) {
-      this.images.add(image);
-    }
+    images.add(image);
     if (update) {
       MapillaryLayer.getInstance().invalidate();
     }
@@ -104,13 +102,11 @@ public class MapillaryData {
   /**
    * Adds a set of {link MapillaryAbstractImage} objects to this object.
    *
-   * @param images The set of images to be added.
+   * @param newImages The set of images to be added.
    * @param update Whether the map must be updated or not.
    */
-  public void addAll(Collection<? extends MapillaryAbstractImage> images, boolean update) {
-    synchronized (this.images) {
-      this.images.addAll(images);
-    }
+  public void addAll(Collection<? extends MapillaryAbstractImage> newImages, boolean update) {
+    images.addAll(newImages);
     if (update) {
       MapillaryLayer.getInstance().invalidate();
     }
@@ -172,9 +168,7 @@ public class MapillaryData {
    * @param image The {@link MapillaryAbstractImage} that is going to be deleted.
    */
   public void remove(MapillaryAbstractImage image) {
-    synchronized (images) {
-      this.images.remove(image);
-    }
+    images.remove(image);
     if (getMultiSelectedImages().contains(image)) {
       setSelectedImage(null);
     }
@@ -227,9 +221,7 @@ public class MapillaryData {
    * @return A Set object containing all images.
    */
   public Set<MapillaryAbstractImage> getImages() {
-    synchronized (images) {
-      return this.images;
-    }
+    return images;
   }
 
   /**
@@ -237,9 +229,7 @@ public class MapillaryData {
    * @return all sequences that are contained in the Mapillary data
    */
   public Set<MapillarySequence> getSequences() {
-    synchronized (images) {
-      return images.stream().map(MapillaryAbstractImage::getSequence).collect(Collectors.toSet());
-    }
+    return images.stream().map(MapillaryAbstractImage::getSequence).collect(Collectors.toSet());
   }
 
   /**
@@ -398,9 +388,9 @@ public class MapillaryData {
    * @param images the new image list (previously set images are completely replaced)
    */
   public void setImages(Collection<MapillaryAbstractImage> images) {
-    synchronized (this.images) {
-      this.images.clear();
-      this.images.addAll(images);
+    synchronized (this) {
+      images.clear();
+      images.addAll(images);
     }
   }
 }

src/org/openstreetmap/josm/plugins/mapillary/MapillaryAbstractImage.java
@@ -261,10 +261,12 @@ public abstract class MapillaryAbstractImage implements Comparable<MapillaryAbst
    *   Call {@link MapillarySequence#add(MapillaryAbstractImage)} first.
    */
   public void setSequence(final MapillarySequence sequence) {
-    if (sequence != null && !sequence.getImages().contains(this)) {
-      throw new IllegalArgumentException();
+    synchronized (this) {
+      if (sequence != null && !sequence.getImages().contains(this)) {
+        throw new IllegalArgumentException();
+      }
+      this.sequence = sequence;
     }
-    this.sequence = sequence;
   }
 
   /**

src/org/openstreetmap/josm/plugins/mapillary/gui/layer/MapObjectLayer.java
@@ -81,20 +81,22 @@ public final class MapObjectLayer extends Layer implements ZoomChangeListener {
   }
 
   public boolean isDownloadRunnableScheduled() {
-    return nextDownloadRunnable != null;
+    synchronized (this) {
+      return nextDownloadRunnable != null;
+    }
   }
 
   public void finishDownload(boolean replaceMapObjects) {
-    final MapObjectDownloadRunnable currentRunnable = downloadRunnable;
-    if (currentRunnable != null) {
-      synchronized (objects) {
-        if (replaceMapObjects) {
-          objects.clear();
-        }
-        objects.addAll(currentRunnable.getMapObjects());
-      }
-    }
     synchronized (this) {
+      final MapObjectDownloadRunnable currentRunnable = downloadRunnable;
+      if (currentRunnable != null) {
+        synchronized (objects) {
+          if (replaceMapObjects) {
+            objects.clear();
+          }
+          objects.addAll(currentRunnable.getMapObjects());
+        }
+      }
       downloadRunnable = null;
       if (nextDownloadRunnable != null) {
         downloadRunnable = nextDownloadRunnable;
@@ -130,9 +132,11 @@ public final class MapObjectLayer extends Layer implements ZoomChangeListener {
   public void paint(Graphics2D g, MapView mv, Bounds bbox) {
     final long startTime = System.currentTimeMillis();
     final Collection<MapObject> displayedObjects = new HashSet<>();
-    final MapObjectDownloadRunnable currentRunnable = downloadRunnable;
-    if (currentRunnable != null) {
-      displayedObjects.addAll(currentRunnable.getMapObjects());
+    synchronized (this) {
+      final MapObjectDownloadRunnable currentRunnable = downloadRunnable;
+      if (currentRunnable != null) {
+        displayedObjects.addAll(currentRunnable.getMapObjects());
+      }
     }
     displayedObjects.addAll(objects);
 

commit 3311f2b9d5ef3cbb5632100284cf97e8d9aacfc9

Fix issue where on a collection the addAll() method is called with the collection itself as argument
This issue has been introduced in e9ed8ba3b1ec96e8824894e0567dc0377ef32703.
There I removed an important `this.`, which then broke the build (see https://josm.openstreetmap.de/jenkins/job/JOSM-Plugins/jdk=JDK8/992/console).

src/org/openstreetmap/josm/plugins/mapillary/MapillaryData.java
@@ -387,10 +387,10 @@ public class MapillaryData {
    *
    * @param images the new image list (previously set images are completely replaced)
    */
-  public void setImages(Collection<MapillaryAbstractImage> images) {
+  public void setImages(Collection<MapillaryAbstractImage> newImages) {
     synchronized (this) {
       images.clear();
-      images.addAll(images);
+      images.addAll(newImages);
     }
   }
 }