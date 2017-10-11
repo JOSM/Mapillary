8b2f5d08b525e6b1e8ee999c9fdb1c2a5f519047
Tobias Ollmann <tobias@mapillary.com>
2017-10-11 15:14:54 +0200

    feat(trafficsign): fill in default value if mapping is missing

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var fs = require('fs');
 
 var mappingJson = fs.readFileSync('./mapping.json', 'utf8');
+var signs = fs.readdirSync('./package_signs');
+
+var mapping = JSON.parse(mappingJson);
+
+signs.map((signFilename) => {
+    var signName = signFilename.split('.')[0]
+    if (mapping[signName] === undefined) {
+        mapping[signName] = {
+            'regions': ['eu', 'us'],
+        };
+    }
+});
 
 module.exports = {
-    mapping: JSON.parse(mappingJson),
+    mapping: mapping,
 };
