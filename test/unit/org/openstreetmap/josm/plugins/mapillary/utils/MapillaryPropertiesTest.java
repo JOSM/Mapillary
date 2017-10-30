5f17d77f59b4ef8e29cecc7a3ec50db6924ec5f5
Florian Schäfer <florian@schaeferban.de>
2017-10-30 20:42:05 +0100

    Add JOSMTestRules to MapillaryPropertiesTest
    This ensures, that AbstractProperty.preferences is set to a non-null value, so a NullPointerException won't occur.
    Finally fixes #68.

--- test/unit/org/openstreetmap/josm/plugins/mapillary/utils/MapillaryPropertiesTest.java
+++ test/unit/org/openstreetmap/josm/plugins/mapillary/utils/MapillaryPropertiesTest.java
@@ -1,9 +1,16 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.mapillary.utils;
 
+import org.junit.Rule;
 import org.junit.Test;
 
+import org.openstreetmap.josm.plugins.mapillary.utils.TestUtil.MapillaryTestRules;
+import org.openstreetmap.josm.testutils.JOSMTestRules;
+
 public class MapillaryPropertiesTest {
+
+  @Rule
+  public JOSMTestRules rules = new MapillaryTestRules();
 
   @Test
   public void test() {
