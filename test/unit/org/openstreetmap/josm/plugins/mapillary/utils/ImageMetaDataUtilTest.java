0216a34ff8df887d63255cac09b4a447c3f2be1a
Hiroshi Miura <miurahr@linux.com>
2019-08-08 20:45:17 +0900

    Add XMP data utility
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ test/unit/org/openstreetmap/josm/plugins/mapillary/utils/ImageMetaDataUtilTest.java
@@ -0,0 +1,39 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins.mapillary.utils;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+
+public class ImageMetaDataUtilTest {
+  @Test
+  public void testXmpXmlParse() throws IOException, URISyntaxException {
+    boolean pano = ImageMetaDataUtil.checkXmpProjectionType(new String(Files.readAllBytes(Paths.get(ImageMetaDataUtil.class.getResource(
+      "/xmpTestImages/xmpProjectionOnly.xml").toURI())), StandardCharsets.UTF_8), "equirectangular");
+    assertTrue(pano);
+  }
+
+  @Test
+  public void testImageFileXMP() throws URISyntaxException {
+    final File xmpImageFile = new File(ImageMetaDataUtil.class.getResource("/xmpTestImages/xmpProjectionOnly.jpg").toURI());
+    boolean pano = ImageMetaDataUtil.isPanorama(xmpImageFile);
+    assertTrue(pano);
+  }
+
+  @Test
+  public void testImageStreamingXMP() throws URISyntaxException, FileNotFoundException {
+    final FileInputStream xmpImageStream = new FileInputStream(new File(ImageMetaDataUtil.class.getResource("/xmpTestImages/xmpProjectionOnly.jpg").toURI()));
+    boolean pano = ImageMetaDataUtil.isPanorama(xmpImageStream);
+    assertTrue(pano);
+  }
+}
