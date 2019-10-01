2b1a5766353f7eb97f8de7e7b624c411b59db4f3
Jerome Pietri <jerome@mapillary.com>
2019-10-01 11:18:30 +0200

    fix(change image location): incomplete Mapillary changeset

--- test/unit/org/openstreetmap/josm/plugins/mapillary/utils/api/JsonLocationChangesetEncoderTest.java
+++ test/unit/org/openstreetmap/josm/plugins/mapillary/utils/api/JsonLocationChangesetEncoderTest.java
@@ -1,19 +1,16 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.mapillary.utils.api;
 
-import static org.junit.Assert.assertEquals;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
-import java.util.Comparator;
-import java.util.Map.Entry;
-
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
-import javax.json.JsonValue;
-
+import org.json.JSONException;
 import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.plugins.mapillary.MapillaryImage;
@@ -21,47 +18,75 @@ import org.openstreetmap.josm.plugins.mapillary.MapillaryLocationChangeset;
 import org.openstreetmap.josm.plugins.mapillary.utils.TestUtil;
 
 public class JsonLocationChangesetEncoderTest {
+
   @Test
-  public void test() throws IllegalArgumentException {
-    MapillaryImage img1 = new MapillaryImage("wMAqAFr3xE9072G8Al6WLQ", new LatLon(0, 0), 0, false);
-    img1.move(13.3323, 50.44612);
-    img1.turn(273.3);
-    img1.stopMoving();
-    MapillaryImage img2 = new MapillaryImage("7erPn382xDMtmfdh0xtvUw", new LatLon(0, 0), 0, false);
-    img2.move(13.3328, 50.44619);
-    img2.stopMoving();
-    MapillaryImage img3 = new MapillaryImage("31KDbCOzla0fJBtIeoBr1A", new LatLon(0, 0), 0, false);
-    img3.turn(13.4);
-    img3.stopMoving();
-    MapillaryImage img4 = new MapillaryImage("invalid image key will be ignored", new LatLon(0, 0), 0, false);
-    img4.turn(13.4);
-    img4.stopMoving();
+  public void testSingleImageChangeset() throws JSONException, URISyntaxException {
+    MapillaryImage image = new MapillaryImage("wMAqAFr3xE9072G8Al6WLQ", new LatLon(50.44612, 13.3323), 10.0, false);
+    image.move(0.1111, 0.22222);
+    image.turn(273.3);
+    image.stopMoving();
 
-    MapillaryLocationChangeset cs = new MapillaryLocationChangeset();
-    cs.add(img1);
-    cs.add(img2);
-    cs.add(img3);
-    cs.add(img4);
-
-    assertEquals(
-      sortChanges(Json.createReader(JsonLocationChangesetEncoderTest.class.getResourceAsStream("/api/v3/requests/changeset.json")).readObject()),
-      sortChanges(JsonLocationChangesetEncoder.encodeLocationChangeset(cs).build())
-    );
+    String expected = readLines("/api/v3/requests/single_image_changeset.json");
+    String actual = JsonLocationChangesetEncoder.encodeImageChanges(image).build().toString();
+    JSONAssert.assertEquals(expected, actual, true);
   }
 
-  private static JsonObject sortChanges(JsonObject changeset) {
-    final JsonObjectBuilder result = Json.createObjectBuilder();
-    for (Entry<String, JsonValue> e : changeset.entrySet()) {
-      if ("changes".equals(e.getKey())) {
-        final JsonArray changes = (JsonArray) e.getValue();
-        final JsonArrayBuilder sortedChanges = Json.createArrayBuilder();
-        changes.stream().sorted(Comparator.comparing(o -> ((JsonObject) o).getString("image_key"))).forEachOrdered(sortedChanges::add);
-        result.add(e.getKey(), sortedChanges);
-      } else {
-        result.add(e.getKey(), e.getValue());
-      }
+  @Test
+  public void testTranslationOnlyChangeset() throws JSONException, URISyntaxException {
+    MapillaryImage image = new MapillaryImage("translationOnlyChangesetImageKey", new LatLon(50.44612, 13.3323), 10.0, false);
+    image.move(0.1111, 0.22222);
+    image.stopMoving();
+
+    String expected = readLines("/api/v3/requests/translation_only_changeset.json");
+    String actual = JsonLocationChangesetEncoder.encodeImageChanges(image).build().toString();
+    JSONAssert.assertEquals(expected, actual, true);
+  }
+
+  @Test
+  public void testRotationOnlyChangeset() throws JSONException, URISyntaxException {
+    MapillaryImage image = new MapillaryImage("rotationOnlyChangesetImageKey", new LatLon(50.44612, 13.3323), 10.0, false);
+    image.turn(-80.3);
+    image.stopMoving();
+
+    String expected = readLines("/api/v3/requests/rotation_only_changeset.json");
+    String actual = JsonLocationChangesetEncoder.encodeImageChanges(image).build().toString();
+    JSONAssert.assertEquals(expected, actual, true);
+  }
+
+  private static String readLines(String filePath) throws URISyntaxException {
+    StringBuilder contentBuilder = new StringBuilder();
+    try (Stream<String> stream = Files.lines( Paths.get(JsonLocationChangesetEncoderTest.class.getResource(filePath).toURI()), StandardCharsets.UTF_8))
+    {
+      stream.forEach(s -> contentBuilder.append(s).append("\n"));
     }
-    return result.build();
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+    return contentBuilder.toString();
+  }
+
+  @Test
+  public void testMultipleImagesChangeset() throws IllegalArgumentException, URISyntaxException, JSONException {
+    MapillaryImage image1 = new MapillaryImage("wMAqAFr3xE9072G8Al6WLQ", new LatLon(50.44612, 13.3323), 10.0, false);
+    image1.move(0.1111, 0.22222);
+    image1.turn(273.3);
+    image1.stopMoving();
+    MapillaryImage image2 = new MapillaryImage("7erPn382xDMtmfdh0xtvUw", new LatLon(0, 0), 0, false);
+    image2.move(13.3328, 50.44619);
+    image2.stopMoving();
+    MapillaryImage image3 = new MapillaryImage("invalid image key will be ignored", new LatLon(0, 0), 0, false);
+    image3.turn(13.4);
+    image3.stopMoving();
+
+    MapillaryLocationChangeset changeset = new MapillaryLocationChangeset();
+    changeset.add(image1);
+    changeset.add(image2);
+    changeset.add(image3);
+
+    String expected = readLines("/api/v3/requests/changeset.json");
+    String actual = JsonLocationChangesetEncoder.encodeLocationChangeset(changeset).build().toString();
+    JSONAssert.assertEquals(expected, actual, false);
   }
 
   @Test
