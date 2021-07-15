17d5b3d13536333cf10992bd82b65344817d056f
Taylor Smock <tsmock@fb.com>
2021-07-15 07:23:32 -0600

    CachesTest: Ensure that preferences are initialized
    Signed-off-by: Taylor Smock <tsmock@fb.com>

--- test/unit/org/openstreetmap/josm/plugins/mapillary/cache/CachesTest.java
+++ test/unit/org/openstreetmap/josm/plugins/mapillary/cache/CachesTest.java
@@ -1,11 +1,16 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.mapillary.cache;
 
-import org.junit.jupiter.api.Test;
-
 import org.openstreetmap.josm.plugins.mapillary.utils.TestUtil;
+import org.openstreetmap.josm.testutils.JOSMTestRules;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 class CachesTest {
+  // Preferences are needed to get the cache directories
+  @RegisterExtension
+  JOSMTestRules rule = new JOSMTestRules().preferences();
 
   @Test
   void testUtilityClass() {
