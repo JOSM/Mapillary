16a8557df9d6476952d0269f901c86bebb53ccce
Taylor Smock <tsmock@fb.com>
2021-07-27 13:48:48 -0600

    DataMouseListener: Use pixel distances instead of a latlon buffer
    Signed-off-by: Taylor Smock <tsmock@fb.com>

--- src/main/java/org/openstreetmap/josm/plugins/mapillary/gui/DataMouseListener.java
+++ src/main/java/org/openstreetmap/josm/plugins/mapillary/gui/DataMouseListener.java
@@ -1,8 +1,12 @@
 package org.openstreetmap.josm.plugins.mapillary.gui;
 
+import static org.openstreetmap.josm.data.projection.Ellipsoid.WGS84;
+
+import org.openstreetmap.josm.data.coor.ILatLon;
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.osm.BBox;
 import org.openstreetmap.josm.data.osm.IPrimitive;
+import org.openstreetmap.josm.data.projection.ProjectionRegistry;
 import org.openstreetmap.josm.data.vector.VectorNode;
 import org.openstreetmap.josm.data.vector.VectorWay;
 import org.openstreetmap.josm.gui.MainApplication;
@@ -12,6 +16,7 @@ import org.openstreetmap.josm.plugins.mapillary.gui.layer.MapillaryVectorTileWor
 import org.openstreetmap.josm.plugins.mapillary.gui.layer.PointObjectLayer;
 import org.openstreetmap.josm.plugins.mapillary.utils.MapillaryProperties;
 import org.openstreetmap.josm.tools.Destroyable;
+import org.openstreetmap.josm.tools.HiDPISupport;
 
 import javax.swing.SwingUtilities;
 import javax.swing.event.MouseInputAdapter;
@@ -46,11 +51,7 @@ public class DataMouseListener extends MouseInputAdapter implements Destroyable 
         .collect(Collectors.toList());
       if (!nodes.isEmpty()) {
         // This is needed since Mapillary ids are only unique within a tile.
-        if (layer instanceof MapillaryVectorTileWorkarounds) {
-          ((MapillaryVectorTileWorkarounds) layer).setSelected(nodes);
-        } else {
-          layer.getData().setSelected(nodes);
-        }
+        ((MapillaryVectorTileWorkarounds) layer).setSelected(nodes);
         if (layer instanceof MapillaryLayer && nodes.size() == 1 && MapillaryMainDialog.hasInstance()) {
           SwingUtilities.invokeLater(() -> MapillaryMainDialog.getInstance().setImage(nodes.iterator().next()));
         }
@@ -94,9 +95,10 @@ public class DataMouseListener extends MouseInputAdapter implements Destroyable 
   }
 
   private static BBox getSmallBBox(Point point) {
-    double scale = MainApplication.getMap().mapView.getScale();
+    final double scaleInEastNorthUnitsPerPixel = MainApplication.getMap().mapView.getScale();
+    final double metersPerPixel = ProjectionRegistry.getProjection().getMetersPerUnit() * scaleInEastNorthUnitsPerPixel;
     // This is ~z5
-    if (scale > 10) {
+    if (metersPerPixel > 1) {
       return null;
     }
 
@@ -104,7 +106,10 @@ public class DataMouseListener extends MouseInputAdapter implements Destroyable 
     final BBox bbox = new BBox();
     // TODO fiddle with the scale
     // ~.07 at z12
-    bbox.addLatLon(latLon, 0.0001 * scale);
+    bbox.add(latLon);
+    final double pixelBuffer = HiDPISupport.getHiDPIScale() * 5;
+    bbox.add(getLatLon(latLon, Math.PI / 4, metersPerPixel * pixelBuffer));
+    bbox.add(getLatLon(latLon, 5 * Math.PI / 4, metersPerPixel * pixelBuffer));
     return bbox;
   }
 
@@ -114,5 +119,26 @@ public class DataMouseListener extends MouseInputAdapter implements Destroyable 
       MainApplication.getMap().mapView.removeMouseListener(this);
       MainApplication.getMap().mapView.removeMouseMotionListener(this);
     }
+  }
+
+  /**
+   * Create a new LatLon at a specified distance. Uses WGS84.
+   * NOTE: Copied from another plugin (unreleased). TODO: Add to Geometry?
+   *
+   * @param original The originating point
+   * @param angle The angle (from true north) in radians
+   * @param offset The distance to the new point (meters)
+   * @return The new latlon
+   */
+  private static ILatLon getLatLon(final ILatLon original, final double angle, final double offset) {
+    final double deltaLongitudeDegree = (Math.PI * WGS84.a * Math.cos(Math.toRadians(original.lat())))
+      / (180 * Math.sqrt(1 - WGS84.e2 * Math.pow(Math.sin(Math.toRadians(original.lat())), 2)));
+    final double deltaLatitudeDegree = (Math.PI * WGS84.a * (1 - WGS84.e2))
+      / (180 * Math.pow(1 - WGS84.e2 * Math.pow(Math.sin(original.lat()), 2), 1.5));
+    final double dx = offset * Math.sin(angle);
+    final double dy = offset * Math.cos(angle);
+    final double dLon = dx / deltaLongitudeDegree;
+    final double dLat = dy / deltaLatitudeDegree;
+    return new LatLon(original.lat() + dLat, original.lon() + dLon);
   }
 }
