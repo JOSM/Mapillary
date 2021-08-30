ba27b2cb15cfdf2ec58da4378c426dace08f73b7
Taylor Smock <tsmock@fb.com>
2021-08-30 14:48:41 -0600

    CachesTest: Remove dependency on JOSMTestRules
    Signed-off-by: Taylor Smock <tsmock@fb.com>

--- test/unit/org/openstreetmap/josm/plugins/mapillary/cache/CachesTest.java
+++ test/unit/org/openstreetmap/josm/plugins/mapillary/cache/CachesTest.java
@@ -1,17 +1,16 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.mapillary.cache;
 
-import org.openstreetmap.josm.plugins.mapillary.utils.TestUtil;
-import org.openstreetmap.josm.testutils.JOSMTestRules;
-
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
+import org.openstreetmap.josm.plugins.mapillary.utils.TestUtil;
+import org.openstreetmap.josm.testutils.annotations.BasicPreferences;
 
+/**
+ * Test class for {@link Caches}
+ */
+// Preferences are needed to get the cache directories
+@BasicPreferences
 class CachesTest {
-    // Preferences are needed to get the cache directories
-    @RegisterExtension
-    JOSMTestRules rule = new JOSMTestRules().preferences();
-
     @Test
     void testUtilityClass() {
         TestUtil.testUtilityClass(Caches.class);
