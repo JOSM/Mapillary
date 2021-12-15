2a54585089f07849a766b5470a233a72f9edc3e6
Taylor Smock <tsmock@fb.com>
2021-12-15 15:52:25 -0700

    Dependency updates
    Signed-off-by: Taylor Smock <tsmock@fb.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,9 @@ import kotlin.reflect.full.starProjectedType
 
 plugins {
   id("application")
-  id("com.diffplug.spotless") version "6.0.0"
-  id("com.github.ben-manes.versions") version "0.39.0"
-  id("com.github.spotbugs") version "4.8.0"
+  id("com.diffplug.spotless") version "6.2.0"
+  id("com.github.ben-manes.versions") version "0.41.0"
+  id("com.github.spotbugs") version "5.0.5"
   id("net.ltgt.errorprone") version "2.0.2"
   id("org.openstreetmap.josm") version "0.7.1"
   id("org.sonarqube") version "3.3"
@@ -57,13 +57,14 @@ java.targetCompatibility = JavaVersion.VERSION_1_8
 
 val versions = mapOf(
   "awaitility" to "4.1.1",
-  "errorprone" to "2.10.0",
+  // Errorprone 2.11 requires Java 11+
+  "errorprone" to if (JavaVersion.current() >= JavaVersion.VERSION_11) "2.11.0" else "2.10.0",
   "jdatepicker" to "1.3.4",
   "jmockit" to "1.49",
   "junit" to "5.8.2",
   "pmd" to "6.20.0",
-  "spotbugs" to "4.5.0",
-  "wiremock" to "2.31.0"
+  "spotbugs" to "4.5.3",
+  "wiremock" to "2.32.0"
 )
 
 dependencies {
